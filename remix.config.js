8d212e4d80fc3118887e1a541b8e1b5508ab1c1d
M Haidar Hanif <git@mhaidarhanif.com>
2023-03-02 09:14:56 +0700

    chore: remove comments

--- remix.config.js
+++ remix.config.js
@@ -4,12 +4,7 @@ const isDevelopment = process.env.NODE_ENV === "development";
 
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  /**
-   * When running locally in development mode,
-   * use either the built-in remix server or Vercel.
-   * This by default does not understand the vercel lambda module format,
-   * so we fallback to the standard build output.
-   */
+
   server: isDevelopment ? undefined : "./server-vercel.js",
   serverBuildPath: isDevelopment ? "build/index.js" : "api/index.js",
   appDirectory: "app",
